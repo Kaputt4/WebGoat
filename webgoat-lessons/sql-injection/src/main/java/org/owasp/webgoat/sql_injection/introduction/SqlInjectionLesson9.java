ca3def9b2562f5362d0ad0f0e3c029a4a4e91dec
Mikiped00 <58269150+Mikiped00@users.noreply.github.com>
2021-03-10 14:21:28 +0100

    Resolved SQL Injection in lesson 12

--- webgoat-lessons/sql-injection/src/main/java/org/owasp/webgoat/sql_injection/introduction/SqlInjectionLesson9.java
+++ webgoat-lessons/sql-injection/src/main/java/org/owasp/webgoat/sql_injection/introduction/SqlInjectionLesson9.java
@@ -58,13 +58,14 @@ public class SqlInjectionLesson9 extends AssignmentEndpoint {
 
     protected AttackResult injectableQueryIntegrity(String name, String auth_tan) {
         StringBuffer output = new StringBuffer();
-        String query = "SELECT * FROM employees WHERE last_name = '" + name + "' AND auth_tan = '" + auth_tan + "'";
+        String query = "SELECT * FROM employees WHERE last_name = ? AND auth_tan = ?";
         try (Connection connection = dataSource.getConnection()) {
             try {
-                Statement statement = connection.createStatement(TYPE_SCROLL_SENSITIVE, CONCUR_UPDATABLE);
+                PreparedStatement statement = connection.prepareStatement(query);
+                statement.setString(1,name);
+                statement.setString(2, auth_tan);
                 SqlInjectionLesson8.log(connection, query);
-                ResultSet results = statement.executeQuery(query);
-                var test = results.getRow() != 0;
+                ResultSet results = statement.executeQuery();
                 if (results.getStatement() != null) {
                     if (results.first()) {
                         output.append(SqlInjectionLesson8.generateTable(results));
